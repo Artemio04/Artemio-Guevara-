3358e472633122397b02d1aec0e9de9ff83f4cba
Artemio04 <113803361+Artemio04@users.noreply.github.com>
2022-09-24 11:57:48 -0600

    lab

---
+++ semana 7/lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace L1_AAGC_1291618
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese su nombre ");
+            string nombre = Console.ReadLine(); 
+
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("soy " + nombre);
+
+            Console.Write("hola mundo ");
+            Console.Write("soy " + nombre);
+            Console.ReadKey(); 
+        }
+    }
+}
+
